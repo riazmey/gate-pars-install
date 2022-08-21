81b9509b91af3d740ce484ca8c080f6e6a3724ba
Ivan Radaev <riazmey@bk.ru>
2022-08-21 22:18:14 +0300

    18

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ if [ "$(requestPasswordSU)" == "${FALSE}" ]; then
 fi
 
 if [ -d "${TEMPORARY_DIR}" ]; then
+    echo "rm ${TEMPORARY_DIR}"
     echo "$ROOT_PASS" | sudo -S rm -rf "${TEMPORARY_DIR}"
 fi
 
